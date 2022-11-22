947dabc752a30f4ce6b05fb003c553f89162b97c
Juan Sanchez <jusanchezme@unal.edu.co>
2022-11-21 20:35:45 -0500

    Level 1

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ import time
 import numpy as np
 import funtions as fn
 import heuristic_search
-
+import copy
 url_game = "https://www.minijuegos.com/embed/diamond-rush"
 chrome_pos = 1475, 1050
 search_bar = 434, 70
@@ -60,11 +60,13 @@ def get_movements(matrix):
 
 print('****')
 
+
 def euclidian_dist(pos_ch, elem_list):
     dist = []
     for i in elem_list:
         dist.append(np.sqrt(np.sum(np.square(np.array(pos_ch) - np.array(i)))))
     return dist
+
 
 def node_swap(node, pos_x, pos_y, operX, operY):
     node[pos_x][pos_y] = node[pos_x + operX][pos_y + operY]
@@ -72,10 +74,84 @@ def node_swap(node, pos_x, pos_y, operX, operY):
     return node
 
 
+# Matrix, currentPosition, [objetivos], [distancias euclidianas], LIM_MOV
+# diccionario: posicion
+
+def getPath(Matrix, currentPosition, objetivos, distancias, LIM_MOV):
+    # 1. obtener objetivo más cercano.
+    nearest_objetive = objetivos.index(min(distancias))
+    # 2. Ir al objetivo
 
 
+def goTo(Matrix, currentPosition, objetivos, distancias, LIM_MOV, Result):
+
+    row = currentPosition[0]
+    col = currentPosition[1]
+
+    print(Result)
+    if Matrix[row][col] == "sT" and (len(objetivos) == 0):
+        return Result
+    if LIM_MOV == 0:
+        return []
+
+    # TODO Verificar si ya estoy en uno de los objetivos:
+    if Matrix[row][col] == "d":
+        toRemove = objetivos.index([row, col])
+        objetivos.pop(toRemove)
+
+    # Movimiento hacia arriba
+    if isMovValid(Matrix[row - 1][col]) and Result[-1] != "down":
+        # TODO calcular distancias
+        print("en up")
+        nextResult = copy.deepcopy(Result)
+        nextResult.append("up")
+        print(nextResult)
+        print("---------")
+        nextPosition = [row - 1, col]
+        nextDistances = euclidian_dist(nextPosition, objetivos)
+        Up = goTo(Matrix, nextPosition, objetivos,
+                  nextDistances, LIM_MOV - 1, nextResult)
+        return Up
+
+    # Movimiento hacia abajo
+    if isMovValid(Matrix[row + 1][col]) and Result[-1] != "up":
+        nextPosition = [row + 1, col]
+        nextDistances = euclidian_dist(nextPosition, objetivos)
+        return [] + goTo(Matrix, nextPosition, objetivos,
+                         nextDistances, LIM_MOV - 1, Result.append("down"))
+    # Movimiento hacia derecha
+    if isMovValid(Matrix[row][col + 1]) and Result[-1] != "left":
+        nextPosition = [row, col + 1]
+        nextDistances = euclidian_dist(nextPosition, objetivos)
+        return goTo(Matrix, nextPosition, objetivos,
+                    nextDistances, LIM_MOV - 1, Result.append("right"))
+
+    # Movimiento hacia derecha
+    if isMovValid(Matrix[row][col - 1]) and Result[-1] != "right":
+        nextPosition = [row, col - 1]
+        nextDistances = euclidian_dist(nextPosition, objetivos)
+        return goTo(Matrix, nextPosition, objetivos,
+                    nextDistances, LIM_MOV - 1, Result.append("left"))
+
+    return ["No hay caminos"]
+
+
+def isMovValid(Mov):
+    return Mov != "NF" and Mov != "W1" and Mov != "W2"
+
+
+[1, 2, 0.5, 2]
 # aux = get_elements(matrix, 'd')
 # print(aux)
+
+
+def menor(lista):
+    min = lista[0]
+    for x in lista:
+        if x < min:
+            min = x
+    return min
+
 
 diams = get_elements(matrix, 'd')
 keys = get_elements(matrix, 'k')
@@ -100,6 +176,12 @@ print(get_movements(matrix))
 dist = euclidian_dist(get_ch_pos(matrix), diams)
 print(dist)
 
+print(type(goTo(
+    matrix,
+    [get_ch_pos(matrix)[0], get_ch_pos(matrix)[1]],
+    diams,
+    dist, get_movements(matrix),
+    [""])))
 # fn.press_and_release('right', pressed=5)
 # fn.press_and_release('down', pressed=3)
 # fn.press_and_release('left', pressed=5)
